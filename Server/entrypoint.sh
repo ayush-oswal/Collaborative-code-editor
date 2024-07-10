4b114854d0f78670cf85d7d9fd104970da1694a5
ayush-oswal <ayushoswal2003@gmail.com>
2024-07-10 13:53:54 +0530

    Resolved docker build conflict, removed volumes

--- Server/entrypoint.sh
+++ Server/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Check if node_modules directory does not exist
 if [ ! -d "node_modules" ]; then
+  # Install dependencies if node_modules is missing
   npm install
 fi
 
+# Execute the CMD passed as arguments to the script
 exec "$@"
